896f2f26dd863bcb492f2f119670146e7d0e4649
DESKTOP-B4HNOQR\paste <pastel07balloo@gmail.com>
2022-09-12 02:11:02 +0900

    背景設定/バーの位置がずれるバグ対処

--- AutumnProject/Assets/Scripts/BarMover.cs
+++ AutumnProject/Assets/Scripts/BarMover.cs
@@ -7,13 +7,19 @@ using UnityEngine;
 public class BarMover : MonoBehaviour
 {
     [SerializeField] private bool _barMove = false;
+    private float _startTime = 0f;
     
+    private void Start()
+    {
+        _startTime = Time.time;
+    }
+
     // Update is called once per frame
     private void Update()
     {
         if (_barMove)
         {
-            this.transform.position += Vector3.up * Mathf.Sin(Time.time)*Time.deltaTime;
+            this.transform.position += Vector3.up * Mathf.Sin(Time.time-_startTime)*Time.deltaTime;
         }
     }
 }
